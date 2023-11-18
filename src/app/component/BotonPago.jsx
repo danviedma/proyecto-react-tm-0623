5b3a55a3d61af828e4a6f1f8b9bfad00e5253d8b
Alejandro Egaña <danviedma@gmail.com>
2023-11-18 14:34:04 -0300

    se agrega pagina galeria

---
+++ src/app/component/BotonPago.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BotonPago() {
+  return (
+    <div>
+      <button className="flex p-3 mb-2 text-white bg-yellow-500 border-2 rounded-xl w-100 h-30 hover:bg-yellow-700">
+        Comprar
+      </button>
+    </div>
+  );
+}
+
+export default BotonPago;
