79dd7fb265033c0dca0b45810412c00039f5121b
Pablo Vaserman <pablovaser@yahoo.com.ar>
2023-11-28 14:18:51 -0300

    Creando alerta de contacto

--- src/app/Contacto/page.jsx
+++ src/app/Contacto/page.jsx
@@ -2,18 +2,31 @@ import React from "react";
 
 import Image from "next/image";
 
+import Swal from 'sweetalert2'
+
 import imgcontacto from  "@/app/asset/img/contacto.jpg";
 
 function PaginadeContacto  () {
 
 return (
 
-    <div className="flex justify-center items-center">
-    <Image src={imgcontacto} className="w-[50%]"></Image>
+   //  <div className="flex justify-center items-center">
+    Swal.fire({
+  title: "Recibimos tu solicitud!",
+  text: "Nos comunicaremos en breve!",
+  icon: "success"
+})
 
-    </div>
+    
 )
 
 }
 
 export default PaginadeContacto
+
+
+// Swal.fire({
+ //   title: "Good job!",
+ //   text: "You clicked the button!",
+ //   icon: "success"
+ //  });   
