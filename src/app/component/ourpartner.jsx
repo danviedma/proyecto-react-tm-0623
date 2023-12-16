2dc36df537d55ec5022e81782bda98d39d9b3bb8
Enomao86 <135537550+Enomao86@users.noreply.github.com>
2023-12-16 13:04:34 -0300

    saco sales entra libro, se grega icono libros en solapa de pagina

--- src/app/component/ourpartner.jsx
+++ src/app/component/ourpartner.jsx
@@ -7,29 +7,29 @@ function Ourpartner() {
       className="mb-5 bg-center bg-cover "
       style={{
         backgroundImage:
-          "url('https://alberdisa.vteximg.com.br/arquivos/ids/167873/Fiambres%20y%20Embutidos.png?v=637654937046300000')",
+          "url('https://el-libro.org.ar/inicio-fel/images/separador.svg')",
       }}
     >
       <h3
         className="mb-2 font-extrabold text-center text-yellow-600 shadow-black"
         style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)" }}
       >
-        Nuestras recomendaciones regionales.
+        Paginas amigas de interes.
       </h3>
       <p
         className="mt-2 font-bold text-center text-white "
         style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)" }}
       >
-        Salames caseros regionales, de la ciudad. Calidad y tradición.
+        Ferias del libro.
       </p>
       <div className="text-center">
         <a
-          href="https://www.instagram.com/ellmandamas/"
+          href="https://el-libro.org.ar/inicio-fel/"
           target="_blank"
           rel="el mandamas"
         >
           <button className="px-4 py-2 text-white bg-yellow-600 rounded animate-shake hover:bg-blue-500 animate-bounce ">
-            Obtenga más información
+            Fundación el libro
           </button>
         </a>
       </div>
